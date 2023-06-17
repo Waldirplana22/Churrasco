1ca0c2bb0b8b52a79a77c0a7f93cd3b6d1d75131
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-06-16 22:16:30 -0300

    Churrasco

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Churrasco
+
+{
+    class programa
+
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Clear();
+            Console.WriteLine("-- Churrasco --");
+            Console.WriteLine("");
+
+
+            Console.Write("Digite o número de Adultos que consomem bebidas alcólicas....:");
+            int adultoConsome = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o número de Adulto que não consome bebida alcólicas...:");
+            int adultoNConsome = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o número de crianças................................:");
+            int criancas = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+
+            Console.WriteLine($"Adultos (que consomem bebidas alcoólicas).......:{adultoConsome}");
+            Console.WriteLine($"Adultos (que não consomem bebidas alcoólicas)...:{adultoNConsome}");
+            Console.WriteLine($"crianças .......................................:{criancas}");
+            Console.WriteLine("");
+
+            //CÁLCULO DE CARNE POR PESSOA
+            double carneAd = 400, carneCr = 200, acomp = 200, cerv = 2, refri = 500, agua = 400;
+            double totalCarne = (carneAd * (adultoConsome + adultoNConsome ) + (carneCr * criancas)) / 1000;
+
+            //CÁLCULO PARA ACOMPANHAMENTOS
+            double totalAcomp = (acomp *(adultoConsome + adultoNConsome + criancas)) / 1000;
+
+            //CÁLCULO PARA CERVEJA
+            double totalCerv = (cerv * adultoConsome );
+
+            //EQUAÇÃO PARA REFRIGERANTE
+            double totalRefri = (refri *(adultoNConsome + criancas)) / 1000;
+
+            //CÁLCULO PARA AGUA
+             double totalAgua = (agua *(adultoConsome + adultoNConsome + criancas)) / 1000;
+
+            Console.WriteLine($"Carne............: {totalCarne:n1} Kg");
+            Console.WriteLine($"Acompanhamento...: {totalAcomp:n1} Kg");
+            Console.WriteLine($"Cerveja..........: {totalCerv:n1} L");
+            Console.WriteLine($"Refrigerante.....: {totalRefri:n1} L");
+            Console.WriteLine($"Água.............: {totalAgua:n1} L");
+        }
+    }
+}
